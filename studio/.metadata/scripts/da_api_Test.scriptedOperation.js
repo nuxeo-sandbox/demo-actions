ddff11fea55d715f24047c004a283790b25d44cf
jfletcher <jfletcher@nuxeo.com>
2024-08-15 23:40:59 +0000

    Update Automation Scripting/da_api_Test

--- studio/.metadata/scripts/da_api_Test.scriptedOperation.js
+++ studio/.metadata/scripts/da_api_Test.scriptedOperation.js
@@ -1,4 +1,6 @@
 // This endpoint is used only to validate that the UI is working.
 function run(input, params) {
-  Console.info("Test ok.");
+  var message="Test ok.";
+  Console.info(message);
+  return message;
 }
