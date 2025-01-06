8182bd51f40daa26e67529a723cd66aa78ec4dea
jfletcher <jfletcher@nuxeo.com>
2025-01-06 18:13:00 +0000

    Update Automation Scripting/da_api_TestLong

--- studio/.metadata/scripts/da_api_TestLong.scriptedOperation.js
+++ studio/.metadata/scripts/da_api_TestLong.scriptedOperation.js
@@ -1,6 +1,20 @@
 // This automation script is used only to validate that the UI is working.
 function run(input, params) {
+  var timeTosleepMs = 10000;
+  Console.info("Sleeping for "+timeTosleepMs+"ms...");
+  sleep(timeTosleepMs);
+  Console.info("Done.");
+  
   var message="Test ok.";
   Console.info(message);
+  
   return message;
 }
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  while (new Date().getTime() - start < milliseconds) {
+    // Busy wait
+  }
+}
+
